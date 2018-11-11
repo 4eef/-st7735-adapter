44c2993853a63a0ade307a6047110869b273982f
4eef <4eef@ukr.net>
2018-11-11 13:07:41 +0200

    Initial commit; project was developed and flashed, seems working

---
+++ SW/drivers/src/clock.c
@@ -0,0 +1,37 @@
+/*!****************************************************************************
+* @file    clock.c
+* @author  4eef
+* @version V1.0
+* @date    07.08.2016
+* @brief   --
+*/
+
+/*!****************************************************************************
+* Include
+*/
+#include "clock.h"
+
+/*!****************************************************************************
+* MEMORY
+*/
+
+/*!****************************************************************************
+* @brief    System clock settings initializer
+* @param    
+* @retval   
+*/
+void initClocks(void){
+    RCC->CR2        |= RCC_CR2_HSI14ON;
+    while((RCC->CR2 & RCC_CR2_HSI14RDY) == 0) __NOP();
+    RCC->CR2        |= RCC_CR2_HSI48ON;
+    while((RCC->CR2 & RCC_CR2_HSI48RDY) == 0) __NOP();
+    RCC->CFGR       |= (3 << RCC_CFGR_SW_Pos);
+    while((RCC->CFGR & RCC_CFGR_SWS >> RCC_CFGR_SWS_Pos) != 3) __NOP();
+    FLASH->ACR      |= FLASH_ACR_PRFTBE;
+    while((FLASH->ACR & FLASH_ACR_PRFTBS) == 0) __NOP();
+    FLASH->ACR      |= (1 << FLASH_ACR_LATENCY_Pos);
+    RCC->CFGR3      &= ~RCC_CFGR3_USBSW;
+    RCC->CFGR3      |= RCC_CFGR3_I2C1SW;
+}
+
+/***************** (C) COPYRIGHT ************** END OF FILE ******** 4eef ****/
---
+++ SW/drivers/src/gpio.c
@@ -0,0 +1,129 @@
+/*!****************************************************************************
+* @file     gpio.c
+* @author   Storozhenko Roman - D_EL, 14.08.2016 - 4eef
+* @version  V1.0
+* @date     20.07.2016
+* @date     02.08.2016  fix set nAF
+* @brief    gpio driver for stm32F0
+*/
+
+/*!****************************************************************************
+* Include
+*/
+#include "gpio.h"
+
+/*!****************************************************************************
+* MEMORY
+*/
+pinMode_type   const pinsMode[] = {
+/*0 */  makepin(GPIOB,  8,  floatingInput,              0,  0),     //RES0
+/*1 */  makepin(GPIOF,  0,  floatingInput,              0,  0),     //RES1
+/*2 */  makepin(GPIOF,  1,  floatingInput,              0,  0),     //RES2
+/*3 */  makepin(GPIOA,  0,  floatingInput,              0,  0),     //RES3
+/*4 */  makepin(GPIOA,  1,  outPushPull,                0,  0),     //LCD_Back
+/*5 */  makepin(GPIOA,  2,  outPushPull,                0,  0),     //LCD_CS
+/*6 */  makepin(GPIOA,  3,  outPushPull,                0,  0),     //LCD_DC
+/*7 */  makepin(GPIOA,  4,  outPushPull,                0,  0),     //LCD_RES
+/*8 */  makepin(GPIOA,  5,  alternateFunctionPushPull,  1,  0),     //LCD_SCK
+/*9 */  makepin(GPIOA,  6,  floatingInput,              0,  0),     //RES4
+/*10*/  makepin(GPIOA,  7,  alternateFunctionPushPull,  1,  0),     //LCD_SDA
+/*11*/  makepin(GPIOB,  1,  floatingInput,              0,  0),     //RES5
+/*11*/  makepin(GPIOA,  11, floatingInput,              0,  0),     //RES6
+/*11*/  makepin(GPIOA,  12, floatingInput,              0,  0),     //RES7
+///*14*/  makepin(GPIOA,  13, floatingInput,              0,  0),     //SWDIO
+///*15*/  makepin(GPIOA,  14, floatingInput,              0,  0),     //SWCLK
+};
+uint32_t pinNum = sizeof(pinsMode) / sizeof(pinMode_type);
+
+/*!****************************************************************************
+* InitAllGpio
+*/
+void initGpios(void){
+//    RCC->APB2ENR    |= RCC_APB2ENR_AFIOEN;                      //AFIO
+//#if (JTAG_OFF > 0)
+//    AFIO->MAPR      |= AFIO_MAPR_SWJ_CFG_1; //JTAG
+//#endif
+    pinMode_type *pgpios;
+    pinMode_type *pgpiosEnd;
+    
+    pgpios = (pinMode_type*)pinsMode;
+    pgpiosEnd = pgpios + pinNum;
+    
+    while(pgpios < pgpiosEnd){
+        gppin_init(pgpios->p, pgpios->npin, pgpios->mode, pgpios->iniState, pgpios->nAF);
+        pgpios++;
+    }
+}
+
+/*!****************************************************************************
+*
+*/
+void gppin_init(GPIO_TypeDef *port, uint8_t npin, gpioMode_type mode, uint8_t iniState, uint8_t nAF){
+    //Clock enable
+    if(port == GPIOA)   RCC->AHBENR    |= RCC_AHBENR_GPIOAEN;
+    if(port == GPIOB)   RCC->AHBENR    |= RCC_AHBENR_GPIOBEN;
+    if(port == GPIOC)   RCC->AHBENR    |= RCC_AHBENR_GPIOCEN;
+//    if(port == GPIOD)   RCC->AHBENR    |= RCC_AHBENR_GPIODEN;
+//    if(port == GPIOE)   RCC->AHBENR    |= RCC_AHBENR_GPIOEEN;
+    if(port == GPIOF)   RCC->AHBENR    |= RCC_AHBENR_GPIOFEN;
+//    if(port == GPIOG)   RCC->AHBENR    |= RCC_AHBENR_GPIOGEN;
+//    if(port == GPIOH)   RCC->AHBENR    |= RCC_AHBENR_GPIOHEN;
+//    if(port == GPIOO)   RCC->AHBENR    |= RCC_AHBENR_GPIOOEN;
+    
+    if(iniState != 0){
+        port->BSRR = (1<<npin);
+    }
+    else{
+        port->BRR = (1<<npin);
+    }
+    
+    /*
+    * Clear bit field
+    */
+    port->MODER         &= ~(0x03 << (2 * npin));
+    port->OTYPER        &= ~(1<<npin);
+    port->PUPDR         &= ~(GPIO_RESERVED << (2*npin));
+    port->AFR[npin / 8] &= ~(GPIO_AFRL_AFRL0_Msk << (4*(npin % 8)));
+    
+    switch(mode){
+        case analogMode:
+            port->MODER |= GPIO_ANALOG_MODE << (2*npin);
+            break;
+  
+        case floatingInput:
+            break;
+            
+        case inputWithPullUp:
+            port->PUPDR |= GPIO_PULL_UP << (2*npin);
+            break;
+            
+        case inputWithPullDown:
+            port->PUPDR |= GPIO_PULL_DOWN << (2*npin);
+            break;
+            
+        case outPushPull:
+            port->MODER |= GPIO_GP_OUT << (2*npin);
+            port->OTYPER |= GPIO_PUSH_PULL << npin;
+            break;
+            
+        case outOpenDrain:
+            port->MODER |= GPIO_GP_OUT << (2*npin);
+            port->OTYPER |= GPIO_OPEN_DRAIN << npin;
+            break;
+            
+       case alternateFunctionPushPull:
+            port->MODER |= GPIO_AF_MODE << (2*npin);
+            port->OTYPER |= GPIO_PUSH_PULL << npin;
+            break;
+            
+        case alternateFunctionOpenDrain:
+            port->MODER |= GPIO_AF_MODE << (2*npin);
+            port->OTYPER |= GPIO_OPEN_DRAIN << npin;
+            break;   
+    }
+    
+    //Set number alternate function
+    port->AFR[npin / 8] |= nAF << (4*(npin % 8));
+}
+
+/*************** (C) COPYRIGHT ************** END OF FILE ********* D_EL *****/
